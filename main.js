debb0ca0524db1a84c657fbfd5ba8e8706e2313c
alaaboody65 <alaaboody65@gmail.com>
2021-02-01 01:54:07 +0200

    first app

---
+++ main.js
@@ -0,0 +1,141 @@
+document.getElementById("wow").style.display = "none";
+
+function display() {
+    document.getElementById("wow").style.display = "block";
+}
+
+
+
+/*
+var counter = 0; // 1 2 3 4 5 6 7 8 9 10 
+
+function count() {
+    counter++;
+    document.getElementById("wow").innerHTML = counter;
+
+}
+
+
+
+
+
+
+function whatName() {
+    var yourName = prompt("what is your name ?");
+    var yourName = prompt("what is your name ?");
+    var yourName = prompt("what is your name ?");
+    var yourName = prompt("what is your name ?");
+    var yourName = prompt("what is your name ?");
+
+    document.getElementById("wow").innerHTML = "My Name is" + " <br></br> " + yourName;
+    document.getElementById("wow").style.fontSize = "40px";
+    document.getElementById("wow").style.color = "blue";
+}
+
+
+
+
+
+
+function myFunc(a, b) {
+    return a * b;
+
+};
+
+document.getElementById("wow").innerHTML = myFunc(2, 3);
+
+
+
+function displayEx(a, b) {
+    return a * b;
+}
+
+document.getElementById("wow").innerHTML = displayEx(3, 6);
+
+
+
+function displayWow() {
+    var x = 3;
+    var y = 6;
+    var z = x * y;
+    document.getElementById("wow").innerHTML = z;
+}
+
+displayWow();
+
+
+
+
+
+
+
+
+/* document.getElementById("wow").innerHTML = "Hello Js";
+
+document.getElementById("wow").style.fontSize = "55px";
+
+window.alert("hello");
+
+var x = 5;
+
+console.log(x);
+
+document.write(5 * 6);
+*/
+
+/*
+var هو اعللان عن متغير هخزن فيه قيمة 
+ x = 24; x = varaible
+ 24 = value 
+----------------------------------------------------------
+JavaScript Arithmetic
++ 
+-
+* 
+**
+--
+++
+/ 
+%
+
+
+var firstname = "alaa";
+var FirstName = "alaa2";
+
+var myName = "Abdulrahman";
+    $yourName = "Alaa";
+    $ourName = myName + " " + $yourName;
+    console.log($ourName);
+
+    */
+
+
+/*
+var myName = "Abdulrahman"; // String 
+
+var age = 24; // number
+
+var succses = true; // booloan
+var succses = false; // booloan
+
+var $name = ["Alaa", "mohamed", "Abdelrahman", "Eslam"]; // array
+
+var Car = {
+    name : "ahmed",
+    age : 24,
+    age2 : 36
+} // object
+console.log(Car["age2"]);
+
+var $Zizo; // undfinded
+
+var Zizo = null; // object
+
+== 
+=== 
+
+
+
+
+console.log(typeof $Zizo);
+*/
